b4da15143e9301b512f97e8b2b223f9a68ad1019
Mykhailo Lazoryk <mikolaz2727@gmail.com>
2025-03-18 19:18:17 +0200

    Added some dummy changes

--- faker_demo.py
+++ faker_demo.py
@@ -3,8 +3,12 @@ from faker import Faker
 faker_instance = Faker("UK")
 
 print(faker_instance.first_name())
+
+# SOME OTHER CODE
 print(faker_instance.last_name())
 print(faker_instance.email())
+print(faker_instance.password())
+
 print(faker_instance.phone_number())
 print(faker_instance.password())
 print(faker_instance.profile())
