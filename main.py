a1d546e8735a7a9769b64cab69c6f0c82704b890
Mykhailo Lazoryk <mikolaz2727@gmail.com>
2025-03-18 22:00:35 +0200

    Added lesson 4

--- main.py
+++ main.py
@@ -6,12 +6,32 @@ import httpx
 from random import choices
 from http import HTTPStatus
 
-from flask import Flask, request
+from flask import Flask, request, jsonify, Response
 from webargs.flaskparser import use_kwargs
 
+from database_handler import execute_query
 from validator import password_length_validator
 
 app = Flask(__name__)
+
+
+@app.errorhandler(HTTPStatus.UNPROCESSABLE_ENTITY)
+@app.errorhandler(HTTPStatus.BAD_REQUEST)
+def error_handling(error):
+    headers = error.data.get("headers", None)
+    messages = error.data.get("messages", ["Invalid request."])
+
+    if headers:
+        return jsonify(
+            {"errors": messages},
+            error.code,
+            headers
+        )
+
+    return jsonify(
+        {"errors": messages},
+        error.code
+    )
 
 
 @app.route("/admin")
@@ -54,16 +74,18 @@ def generate_password(length):
     )
     return f"Generated password: {password}"
 
+
 @app.route("/get-astronauts")
 def get_astronauts():
     url = "http://api.open-notify.org/astros.json"
     result = httpx.get(url=url, params={})
 
-    if result.status_code not in (HTTPStatus.OK, ):
-        return "ERROR: Something went wrong with api.open-notify.org"
+    if result.status_code not in (HTTPStatus.OK,):
+        return Response("ERROR: Something went wrong with api.open-notify.org",
+                        status=result.status_code)
 
     result: dict = result.json()
-    statistics = { }
+    statistics = {}
     for entry in result.get('people', []):
         statistics[entry['craft']] = statistics.get(entry['craft'], 0) + 1
 
@@ -71,7 +93,36 @@ def get_astronauts():
 
     return statistics
 
+
+@app.route("/get-customers")
+def get_all_customers():
+    query = "SELECT FirstName, LastName FROM customers"
+
+    records = execute_query(query=query)
+    return records
+
+
 if __name__ == '__main__':
     app.run(
         'localhost', debug=True
     )
+
+# 100 - info
+# 200 - OK
+# 300 - redirect
+# 400 - client
+# 500 - server
+
+# GET - get info
+# params in url
+# search/filter
+
+# POST - change on server
+# has request body
+# passwords/data/files/
+
+# SQL -
+
+# relation !- SQL
+
+# no relation !- SQL
